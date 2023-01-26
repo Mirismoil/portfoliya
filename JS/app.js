72d713aa2b7e25b3063898fce75206ef7598f25d
Mirismoil <yuldashevmirismoil3@gmail.com>
2023-01-27 00:22:50 +0500

    add

--- JS/app.js
+++ JS/app.js
@@ -100,7 +100,6 @@ function changeURLLanguage() {
 function changeLanguage() {
     let hash = window.location.hash;
     hash = hash.substr(1);
-    console.log(hash)
     if (!allLang.includes(hash)) {
         location.href = window.location.pathname + '#ru';
         location.reload();
